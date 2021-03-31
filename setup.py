0ed6385151ba92f1e9822741614822216328ceef
Sam <samuel.molina@enreda.coop>
2021-03-31 12:37:32 +0100

    basic packetization

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='drf-tester',
+      version='0.1',
+      description='Convenient test classes for DjangoRestFramework',
+      url='http://github.com/samuelmovi/drf-tester/',
+      author='Samul Movi',
+      author_email='samuel.software.developer@email.com',
+      license='MIT',
+      packages=['drf_tester',],
+      zip_safe=False)
+
