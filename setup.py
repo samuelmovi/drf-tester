e1451c214392e195442bba6009959ce04ce71c92
Sam <samuel.molina@enreda.coop>
2021-09-08 19:11:38 +0100

    tweak

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-setup(name='drf-tester',
-      version='0.1',
-      description='Convenient test classes for DjangoRestFramework',
-      url='http://github.com/samuelmovi/drf-tester/',
-      author='Samuel Movi',
-      author_email='samuel.software.developer@email.com',
-      license='MIT',
-      packages=['drf_tester',],
-      zip_safe=False
-    )
+setup(
+    name='drf-tester',
+    version='0.1',
+    description='Convenient test classes for DjangoRestFramework',
+    url='http://github.com/samuelmovi/drf-tester/',
+    author='Samuel Movi',
+    author_email='samuel.software.developer@email.com',
+    license='MIT',
+    packages=['drf_tester',],
+    zip_safe=False
+)
 
