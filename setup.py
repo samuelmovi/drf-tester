5ad4cfadbec98f13f73b656c8d690c591d09c216
Sam <samuel.molina@enreda.coop>
2021-11-05 20:03:22 +0000

    fixed setup.py to include all files and subfolders

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='drf-tester',
-    version='0.1.1',
+    version='0.1.2',
     description='Testing Suite for DjangoRestFramework developers with deadlines.',
     url='http://github.com/samuelmovi/drf-tester/',
     author='Samuel Movi',
     author_email='samuel.software.developer@email.com',
     license='MIT',
-    packages=['drf_tester',],
+    packages=find_packages(include=['drf_tester', 'drf_tester.*']),
     install_requires=["Django", "djangorestframework"],
     zip_safe=False,
-    include_package_data=True,
+    # include_package_data=True,
 )
 
